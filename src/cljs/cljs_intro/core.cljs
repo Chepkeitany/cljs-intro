497afe0eacb815ad563c9b1c97fca64ccc29315f
Santei <arerimoi@ona.io>
2015-07-17 14:37:38 +0300

    AR Modified main function

--- src/cljs/cljs_intro/core.cljs
+++ src/cljs/cljs_intro/core.cljs
@@ -6,12 +6,22 @@
 
 (defonce app-state (atom {:text "Hello World!"}))
 
+(defn chart
+  "Render a bar chart using c3"
+   []
+  (.generate js/c3
+    #js
+    {:bindto "#chart"
+     :data #js {:columns #js ["data1" 30 200 100 400 150 250]}}))
+
 (defn main []
-  (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (:text app)))))
-    app-state
-    {:target (. js/document (getElementById "app"))}))
+  chart
+  ; (om/root
+  ;   (fn [app owner]
+  ;     (reify
+  ;       om/IRender
+  ;       (render [_]
+  ;         (dom/h1 nil (:text app)))))
+  ;   app-state
+  ;   {:target (. js/document (getElementById "app"))})
+)
