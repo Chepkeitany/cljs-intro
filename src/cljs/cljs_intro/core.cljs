fd1b7adf822a368bdc3212533dc21538282462cf
Santei <arerimoi@ona.io>
2015-07-20 15:51:11 +0300

    AR Removed charts namespace

--- src/cljs/cljs_intro/core.cljs
+++ src/cljs/cljs_intro/core.cljs
@@ -12,16 +12,15 @@
   (.generate js/c3
     #js
     {:bindto "#chart"
-     :data #js {:columns #js ["data1" 30 200 100 400 150 250 300 400 200 100 30 200]
-     :types #js {:data1 "bar"} 
-     :axes #js {:data1 "y"}}
+     :data #js {:columns #js ["data1" 30 200 100 400 150 250]
+     :type "bar"}
      :bar #js {:width #js {:ratio 0.5}}
      :axis #js {:y #js {:label #js {:text "Signs up during the year"
                                     :position "outer-middle"
-                                    :show true}}
-                :x #js {:label #js {:text "Months of the year"
-                                    :position "outer-middle"
-                                    :show true}}}}))
+                                    :show true}
+                        :show true
+                        :max 400
+                        :min 0}}}))
 
 (defn main []
   (chart))
